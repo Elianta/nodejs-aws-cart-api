7fbd66620692859a16d7a677efa3ccba89603a8b
a.kashina <a.kashina@websecret.by>
2025-03-27 21:15:27 +0100

    feat: integrate prisma with nestjs and set up database connection

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
+import * as dotenv from 'dotenv';
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CartApiStack } from '../lib/cart-api-stack';
 
+dotenv.config();
+
 const app = new cdk.App();
-new CartApiStack(app, 'CartApiStack');
+new CartApiStack(app, 'CartApiStack', {
+  env: {
+    // These will use your AWS CLI configuration
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
--- cdk/lib/cart-api-stack.ts
+++ cdk/lib/cart-api-stack.ts
@@ -2,17 +2,52 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as path from 'path';
 
 export class CartApiStack extends cdk.Stack {
   private cartApiLambda: lambda.Function;
   private api: apigateway.RestApi;
+  private vpc: ec2.IVpc;
+  private lambdaSG: ec2.SecurityGroup;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    this.importVpc();
+    this.createSecurityGroup();
+    this.allowLambdaAccessToRDS();
     this.createLambdaFunctions();
     this.createApiGateway();
     this.createOutputs();
+  }
+
+  private importVpc(): void {
+    this.vpc = ec2.Vpc.fromLookup(this, 'ExistingVPC', {
+      vpcId: process.env.VPC_ID,
+    });
+  }
+
+  private createSecurityGroup(): void {
+    this.lambdaSG = new ec2.SecurityGroup(this, 'LambdaSecurityGroup', {
+      vpc: this.vpc,
+      description: 'Security group for Lambda function',
+      allowAllOutbound: true,
+    });
+  }
+
+  private allowLambdaAccessToRDS(): void {
+    const rdsSecurityGroup = ec2.SecurityGroup.fromSecurityGroupId(
+      this,
+      'RDSSecurityGroup',
+      process.env.RDS_SECURITY_GROUP_ID as string,
+    );
+
+    rdsSecurityGroup.addIngressRule(
+      this.lambdaSG,
+      ec2.Port.tcp(parseInt(process.env.RDS_PORT as string) || 5432),
+      'Allow Lambda to access RDS',
+    );
   }
 
   private createLambdaFunctions(): void {
@@ -20,7 +55,17 @@ export class CartApiStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'lambda.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, '../../dist'), {}),
-      timeout: cdk.Duration.seconds(10),
+      vpc: this.vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      allowPublicSubnet: true,
+      securityGroups: [this.lambdaSG],
+      environment: {
+        DATABASE_URL: `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOST}:${process.env.RDS_PORT}/${process.env.RDS_DATABASE}`,
+        NODE_ENV: 'production',
+      },
+      timeout: cdk.Duration.seconds(30),
     });
   }
 
