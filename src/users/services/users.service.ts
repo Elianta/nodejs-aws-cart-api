445df19060bc3f600362bcf55c28fe1ab272db4f
a.kashina <a.kashina@websecret.by>
2025-03-28 14:18:05 +0100

    feat: add users table and integration with database

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,30 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { randomUUID } from 'node:crypto';
 import { User } from '../models';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  private readonly users: Record<string, User>;
+  constructor(private prisma: PrismaService) {}
 
-  constructor() {
-    this.users = {};
+  async findOne(name: string): Promise<User> {
+    return this.prisma.user.findUnique({ where: { name } });
   }
 
-  findOne(name: string): User {
-    for (const id in this.users) {
-      if (this.users[id].name === name) {
-        return this.users[id];
-      }
-    }
-    return;
-  }
-
-  createOne({ name, password }: User): User {
-    const id = randomUUID();
-    const newUser = { id, name, password };
-
-    this.users[id] = newUser;
-
-    return newUser;
+  async createOne({ name, password }: Omit<User, 'id'>): Promise<User> {
+    return this.prisma.user.create({
+      data: {
+        name,
+        password, // Note: In production, ensure password is hashed before storing
+      },
+    });
   }
 }
